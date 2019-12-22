ca21a2558dd7ff55f662bb533d4021ccb727c33c
techguy940 <51826754+techguy940@users.noreply.github.com>
2019-12-22 18:57:43 +0530

    Update timeBot.py

--- timeBot.py
+++ timeBot.py
@@ -4,18 +4,19 @@ import discord
 from discord.ext import commands
 import asyncio
 
-botPrefix = "BOT_PREFIX"
-botToken = "BOT_TOKEN"
+botPrefix = "time!"
+botToken = "NjU3NjgyMzQ2MzE2NjYwNzQ2.Xf0wbw.Lt3tC7_KspsV8lDWB3hh4DPtRTI"
 
 bot=commands.Bot(command_prefix=botPrefix)
-t = None #change to your timezone like "America/New_York"
+t = "Asia/Dubai"
 timeZone = tz.gettz(t) if t is not None else tz.tzutc()
 
 
 @bot.command()
 async def start(ctx):
-    await ctx.guild.me.edit(nick=datetime.now().astimezone(timeZone).strftime("%a %I:%M %p"))
-    await asyncio.sleep(300) #changes time every 5 minutes
-    await ctx.guild.me.edit(nick=datetime.now().astimezone(timeZone).strftime("%a %I:%M %p"))
+    while True:
+      await ctx.guild.me.edit(nick=datetime.now().astimezone(timeZone).strftime("%a %I:%M %p"))
+      await asyncio.sleep(300) #changes time every 5 minutes
+      await ctx.guild.me.edit(nick=datetime.now().astimezone(timeZone).strftime("%a %I:%M %p"))
 
 bot.run(botToken)
